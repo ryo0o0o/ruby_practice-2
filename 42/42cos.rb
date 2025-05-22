f9f66b7dd9cbffa5ef0098de09e0dc1304058dd0
hirakawa <ryohirakawa040512@gmail.com>
2025-05-22 14:31:50 +0900

    Delete 42 directory

--- 42/42cos.rb
+++
@@ -1,9 +0,0 @@
-open('survocos.dat', 'w') { |f|
-    20.times{|i|
-        x = i / 19.0
-        y = 90 * Math.cos(Math::PI * (x - 1)) + 90
-        f.puts "#{x} #{y}"
-    }
-}
-
-`gnuplot -persist plot_cos.gp`
--- 42/plot_cos.gp
+++
@@ -1,7 +0,0 @@
-set title "サーボモーターの角度" font("Arial,20")
-set xlabel "[0-1]"
-set ylabel "角度[deg]"
-set yrange [0:180]
-set ytics 30
-set terminal qt
-plot 'survocos.dat' w lp
